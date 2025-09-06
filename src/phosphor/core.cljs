63cc6aa18058e29c333c446560f1f62ff28158f8
Vincent Cantin <vincent@404.taipei>
2025-09-07 01:10:15 +0300

    Some fixes

--- src/phosphor/core.cljs
+++ src/phosphor/core.cljs
@@ -1,5 +1,6 @@
 (ns phosphor.core
   (:require ["fs" :as fs]
+            ["path" :as path]
             ["@phosphor-icons/core" :as phosphor-icons]
             [clojure.string :as str]
             [cljs.pprint :as pp]
@@ -21,32 +22,48 @@
                        (update :published-in number->version-str)
                        (update :updated-in number->version-str)))))))
 
+(defn ensure-parent-dirs-exist [filename]
+  (let [directory-path (path/dirname filename)]
+    (when-not (fs/existsSync directory-path)
+      (fs/mkdirSync directory-path #js {:recursive true}))))
+
+(defn write-file-sync [filename content-str]
+  (ensure-parent-dirs-exist filename)
+  (fs/writeFileSync filename content-str))
+
 (defn main [target-directory]
-  (when-not (str/blank? target-directory)
+  (let [target-directory (if (str/blank? target-directory)
+                           "data-out"
+                           target-directory)]
     ;; Export the metadata
-    (fs/writeFileSync (str target-directory "/metadata.edn")
-                      (with-out-str (pp/pprint icons)))
+    (write-file-sync (str target-directory "/metadata.edn")
+                     (with-out-str (pp/pprint icons)))
 
     ;; Export the SVG files to Hiccup format in EDN files.
-    (doseq [icon-style ["bold" "duotone" "fill" "light" "regular" "thin"]]
-      (let [icon-style-folder-path (str target-directory "/" icon-style)]
-        ;; Ensure that the folder for this style exists
-        (when-not (fs/existsSync icon-style-folder-path)
-          (fs/mkdirSync icon-style-folder-path #js {:recursive true}))
-
-        (doseq [{icon-name :name} icons]
-          (let [src-svg-file-path (js/require.resolve (str "@phosphor-icons/core/assets/"
-                                                           icon-style
-                                                           "/"
-                                                           (if (= icon-style "regular")
-                                                             icon-name
-                                                             (str icon-name "-" icon-style))
-                                                           ".svg"))
-                dst-hiccup-file-path (str target-directory "/" icon-style "/" icon-name ".edn")]
-            (-> src-svg-file-path
-                (fs/readFileSync "utf8")
-                (html->hiccup)
-                (as-> hiccup (with-out-str (pp/pprint hiccup)))
-                (->> (fs/writeFileSync dst-hiccup-file-path)))))))
+    (doseq [icon-style ["bold" "duotone" "fill" "light" "regular" "thin"]
+            {icon-name :name} icons]
+      (let [src-svg-file-path (js/require.resolve (str "@phosphor-icons/core/assets/"
+                                                       icon-style
+                                                       "/"
+                                                       (if (= icon-style "regular")
+                                                         icon-name
+                                                         (str icon-name "-" icon-style))
+                                                       ".svg"))
+            svg-content (-> src-svg-file-path
+                            (fs/readFileSync "utf8"))]
+        ;; Export the SVG file
+        (write-file-sync (str target-directory "/" icon-style "/" icon-name ".svg")
+                         svg-content)
+        ;; Export the EDN file
+        (write-file-sync (str target-directory "/" icon-style "/" icon-name ".edn")
+                         (with-out-str (pp/pprint (first (html->hiccup svg-content)))))))
 
     (println (str "Export to directory " target-directory " is finished."))))
+
+(comment
+
+  (-> (js/require.resolve "@phosphor-icons/core/assets/bold/acorn-bold.svg")
+      (fs/readFileSync "utf8")
+      (html->hiccup))
+
+  ,)
